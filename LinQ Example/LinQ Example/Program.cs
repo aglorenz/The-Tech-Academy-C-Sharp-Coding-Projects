a85cbf2c66ed939c622fb80f9a3e5e8550a3ed49
Andrew Lorenz <aglorenz777@gmail.com>
2020-11-21 18:32:16 -0800

    LinQ practice

---
+++ LinQ Example/LinQ Example/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1)  Specify the dta source.
+            //string[] students = new string[]  {"Lacey","Trisha", "Gavin","Josh","Jon","Landon","Kyndreshia"};
+            string[] students = new string[]  {"Lacey","Trisha", "Gavin","Josh","Jon","Landon","Kyndreshia"};
+
+            //Define the query expression.
+            IEnumerable<string> studentQuery =
+                from student in students
+                where student.Length < 4 // only Jon is returned
+                select student;
+
+            //execute the query
+            foreach (string s in studentQuery) // studentQuery holds the results of the query
+            {
+                Console.WriteLine(s + " Length = " + s.Length);
+            }
+            Console.ReadLine();
+        }
+    }
+}
