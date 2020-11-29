0cb91ce80c01786b79f1473c5f3981582223714a
Andrew Lorenz <aglorenz777@gmail.com>
2020-11-29 15:10:08 -0800

    Added the SQL DB connection string

--- MVCNewsLetterApp/MVCNewsLetterApp/Controllers/HomeController.cs
+++ MVCNewsLetterApp/MVCNewsLetterApp/Controllers/HomeController.cs
@@ -12,7 +12,21 @@ namespace MVCNewsLetterApp.Controllers
         {
             return View();
         }
-
+        
+        [HttpPost]
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                string connectionString= @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+                //Now we will connect to our database with ADO.net
+                return View("Success");
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
