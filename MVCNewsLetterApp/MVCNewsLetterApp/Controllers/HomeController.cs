5c97f11ef6e53f6e4f864ff2a7ba1007b2d0a98c
Andrew Lorenz <aglorenz777@gmail.com>
2020-12-01 09:27:01 -0800

    Add ADO

--- MVCNewsLetterApp/MVCNewsLetterApp/Controllers/HomeController.cs
+++ MVCNewsLetterApp/MVCNewsLetterApp/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,7 +26,26 @@ namespace MVCNewsLetterApp.Controllers
             {
                 string connectionString= @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
                 //Now we will connect to our database with ADO.net
-                return View("Success");
+                //Using parameters helps prevent SQL injection
+                string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) 
+                                    VALUES
+                                    (@FirstName, @LastName, @EmailAddress)";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
+                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+
+                    command.Parameters["@FirstName"].Value = firstName;
+                    command.Parameters["@LastName"].Value = lastName;
+                    command.Parameters["@EmailAddress"].Value = emailAddress;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();  // Its an insert so technically, it's a non-query
+                    connection.Close();
+                }
+                    return View("Success");
             }
         }
         public ActionResult About()
