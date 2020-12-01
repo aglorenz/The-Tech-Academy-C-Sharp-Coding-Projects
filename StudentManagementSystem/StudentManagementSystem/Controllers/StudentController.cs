c7169e344c6ad8f10d9e3e4db10bb9fdaca6b665
Andrew Lorenz <aglorenz777@gmail.com>
2020-12-01 15:04:55 -0800

    Updates to Care Insurance MVC and Student Mgmgt System

---
+++ StudentManagementSystem/StudentManagementSystem/Controllers/StudentController.cs
@@ -0,0 +1,152 @@
+﻿using StudentManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string _connectionString = @"Data Source=(localdb)\MSSQLLocalDB;" +
+            "Initial Catalog=School;Integrated Security=True;Connect Timeout=30;Encrypt=False;" +
+            "TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            string queryString = "SELECT * FROM Students";
+            List<Student> students = new List<Student>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Student student = new Student
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        FirstName = reader["FirstName"].ToString(),
+                        LastName = reader["LastName"].ToString()
+                    };
+                    students.Add(student);
+                }
+                connection.Close();
+            }
+            return View(students);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Student student)
+        {
+            string queryString = @"INSERT INTO Students (FirstName, LastName) " +
+                                    "VALUES (@FirstName, @LastName)";
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                command.Parameters.Add("@LastName", SqlDbType.VarChar);
+
+                command.Parameters["@FirstName"].Value = student.FirstName;
+                command.Parameters["@LastName"].Value = student.LastName;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+            return RedirectToAction("Index");
+        }
+
+        // Take in the ID of a student and return the student details to the View.
+        public ActionResult Details(int id)
+        {
+            string queryString = "SELECT * FROM Students WHERE id = @id";
+            Student student = new Student();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@id", SqlDbType.Int);
+
+                command.Parameters["@id"].Value = id;
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    student.Id = Convert.ToInt32(reader["Id"]);
+                    student.FirstName = reader["FirstName"].ToString();
+                    student.LastName = reader["LastName"].ToString();
+                }
+                connection.Close();
+            }
+            return View(student);
+        }
+
+        // Take the Id of the student as a parameter and get that student from the
+        // database and pass it to the View
+        public ActionResult Edit(int id)
+        {
+            string queryString = "SELECT * FROM Students WHERE id = @id";
+            Student student = new Student();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@Id", SqlDbType.Int);
+
+                command.Parameters["@Id"].Value = id;
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    student.Id = Convert.ToInt32(reader["Id"]);
+                    student.FirstName = reader["FirstName"].ToString();
+                    student.LastName = reader["LastName"].ToString();
+                }
+                connection.Close();
+            }
+            return View(student);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+            string queryString = @"UPDATE Students SET FirstName = @FirstName," +
+                " LastName = @LastName WHERE Id = @Id";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@Id", SqlDbType.Int);
+                command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                command.Parameters.Add("@LastName", SqlDbType.VarChar);
+
+                command.Parameters["@Id"].Value = student.Id;
+                command.Parameters["@FirstName"].Value = student.FirstName;
+                command.Parameters["@LastName"].Value = student.LastName;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
