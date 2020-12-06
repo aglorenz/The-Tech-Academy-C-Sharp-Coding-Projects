a038faf899fab534319a6cded2756e777d678f71
Andrew Lorenz <aglorenz777@gmail.com>
2020-12-05 17:30:42 -0800

    Convert to EntityFramework from ADO.NET

---
+++ MVCNewsLetterAppEntityFramework/MVCNewsLetterAppEntityFramework/Controllers/HomeController.cs
@@ -0,0 +1,137 @@
+﻿using MVCNewsLetterAppEntityFramework.Models;
+using MVCNewsLetterAppEntityFramework.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCNewsLetterAppEntityFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                //Using Entity Framework is much easier to access the database than ADO.NET
+                using (NewsletterEntities db = new NewsletterEntities())
+                {
+                    var signup = new SignUp();
+                    signup.FirstName = firstName;
+                    signup.LastName = lastName;
+                    signup.EmailAddress = emailAddress;
+
+                    db.SignUps.Add(signup);
+                    db.SaveChanges();
+                }
+                //Now we will connect to our database with ADO.net
+                //Using parameters helps prevent SQL injection
+                //string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) 
+                //                    VALUES
+                //                    (@FirstName, @LastName, @EmailAddress)";
+                //using (SqlConnection connection = new SqlConnection(connectionString))
+                //{
+                //    SqlCommand command = new SqlCommand(queryString, connection);
+                //    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                //    command.Parameters.Add("@LastName", SqlDbType.VarChar);
+                //    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+
+                //    command.Parameters["@FirstName"].Value = firstName;
+                //    command.Parameters["@LastName"].Value = lastName;
+                //    command.Parameters["@EmailAddress"].Value = emailAddress;
+
+                //    connection.Open();
+                //    command.ExecuteNonQuery();  // Its an insert so technically, it's a non-query
+                //    connection.Close();
+                //}
+                return View("Success");
+            }
+        }
+
+        public ActionResult Admin()
+        {
+
+            using (NewsletterEntities db = new NewsletterEntities()) // this instantiation gives us access to the database
+            {
+
+                //this is the EntityFramework method
+                
+                // as the db grows we will make more specific calls to the database to narrow our list, but for now, we grab all records
+                var signups = db.SignUps;  // represents all the records in the database  This maps to Signups in Newsletter.Context.cs
+                var signupVms = new List<SignupVm>();  // var is best practice if its obvious what the data type is
+                //List<SignupVm> signupVms = new List<SignupVm>();
+
+                // Map our database object to a view model ( so we don't pass personal info to the view)
+                foreach (var signup in signups)
+                {
+                    var signupVm = new SignupVm();
+                    signupVm.FirstName = signup.FirstName;
+                    signupVm.LastName = signup.LastName;
+                    signupVm.EmailAddress = signup.EmailAddress;
+                    signupVms.Add(signupVm);
+                }
+
+                return View(signupVms); // pass the list or records to the view
+            }
+        }
+
+                // Below is the old ADO.NET method.  Notice its longer than the 
+                //string queryString = @"SELECT Id, FirstName, LastName, EmailAddress, SocialSecurityNumber FROM SignUps";
+                //List<NewsletterSignUp> signups = new List<NewsletterSignUp>();
+
+                //using (SqlConnection connection = new SqlConnection(connectionString))
+                //{
+                //    SqlCommand command = new SqlCommand(queryString, connection);
+
+                //    connection.Open();
+
+                //    SqlDataReader reader = command.ExecuteReader();
+
+                //    while (reader.Read())
+                //    {
+                //        var signup = new NewsletterSignUp();
+                //        signup.Id = Convert.ToInt32(reader["Id"]);
+                //        signup.FirstName = reader["FirstName"].ToString();
+                //        signup.LastName = reader["LastName"].ToString();
+                //        signup.EmailAddress = reader["EmailAddress"].ToString();
+                //        signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString(); // wwe don't want to return this
+
+                //        signups.Add(signup);
+                //    }
+                //}
+
+                //-----------------------------------------------------------------------------------
+                //--------------- Above part is the ADO.NET extra code needed above what EF needs -------------------------
+                //-----------------------------------------------------------------------------------
+
+                //    var signupVms = new List<SignupVm>();  // var is best practice if its obvious what the data type is
+                //    //List<SignupVm> signupVms = new List<SignupVm>();
+
+                //    foreach (var signup in signups)
+                //    {
+                //        var signupVm = new SignupVm();
+                //        signupVm.FirstName = signup.FirstName;
+                //        signupVm.LastName = signup.LastName;
+                //        signupVm.EmailAddress = signup.EmailAddress;
+                //        signupVms.Add(signupVm);
+                //    }
+
+                //    return View(signupVms);
+                //}
+
+    }
+}
