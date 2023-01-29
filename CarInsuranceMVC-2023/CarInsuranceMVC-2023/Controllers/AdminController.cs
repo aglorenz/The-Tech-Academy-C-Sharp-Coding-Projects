4c418272edeabc86cbaa7adbf5bd8214ef822bcd
aglorenz <aglorenz777@gmail.com>
2023-01-28 17:14:45 -0800

    New Version of Car Insurance app

---
+++ CarInsuranceMVC-2023/CarInsuranceMVC-2023/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using CarInsuranceMVC_2023.Models;
+using CarInsuranceMVC_2023.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsuranceMVC_2023.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            List<InsureeVM> insureeVMs = new List<InsureeVM>();
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var insurees = db.Insurees;
+                foreach (var insuree in insurees)
+                {
+                    var insureeVM = new InsureeVM();
+                    insureeVM.FirstName = insuree.FirstName;
+                    insureeVM.LastName = insuree.LastName;
+                    insureeVM.EmailAddress = insuree.EmailAddress;
+                    insureeVM.Quote = insuree.Quote;
+                    insureeVMs.Add(insureeVM);
+                }
+            }
+            return View(insureeVMs);
+        }
+    }
+}
---
+++ CarInsuranceMVC-2023/CarInsuranceMVC-2023/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CarInsuranceMVC_2023.MvcApplication" Language="C#" %>
